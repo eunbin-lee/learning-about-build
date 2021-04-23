3e26b14ac30e05e25f694f4b6a50fe6af5361e3e
eunbin-lee <euns2bin@gmail.com>
2021-04-23 19:39:04 +0900

    Webpack ECAMScript Modules (ESM) 예제

--- webpack/module/mathUtil.js
+++ webpack/module/mathUtil.js
@@ -1,15 +1,7 @@
-// [CommonJS 모듈 예제]
+// [Module Keyword 내보내기 (ESM)]
+
 const PI = 3.14;
 const getCircleArea = (r) => r * r * PI;
 
 // 내보내기 방법1
-/* 
-module.exports = {
-    PI,
-    getCircleArea,
-}; 
-*/
-
-// 내보내기 방법2
-exports.PI = PI;
-exports.getCircleArea = getCircleArea;
+export default { PI, getCircleArea };
